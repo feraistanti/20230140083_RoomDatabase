2af45a2dec126153c39cc2558fba896f0a0a4c16
feraistanti <fistanti551@gmail.com>
2025-11-27 16:08:35 +0700

    membuat  DAO untuk akses tabel Siswa

--- app/src/main/java/com/example/a20230140083_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/a20230140083_roomdatabase/room/SiswaDao.kt
@@ -13,6 +13,3 @@ interface SiswaDao {
     @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(siswa: Siswa)
-}
