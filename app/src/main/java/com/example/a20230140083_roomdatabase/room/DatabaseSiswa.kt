7f42316587ec036e3febfc06c0f50f3f04778952
feraistanti <fistanti551@gmail.com>
2025-11-27 16:06:43 +0700

    mengambil instance database

--- app/src/main/java/com/example/a20230140083_roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/a20230140083_roomdatabase/room/DatabaseSiswa.kt
@@ -13,6 +13,14 @@ import androidx.room.RoomDatabase
         @Volatile
         private var Instance: DatabaseSiswa? = null
 
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                    "siswa_database")
+                    .build().also { Instance=it }
+            })
+        }
     }
 
 }
