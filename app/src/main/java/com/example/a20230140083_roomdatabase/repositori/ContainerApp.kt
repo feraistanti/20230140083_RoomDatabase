545dca16c58c7045a550ea7ff7953d94f9e842dd
feraistanti <fistanti551@gmail.com>
2025-11-27 16:01:30 +0700

    Container untuk repo Siswa

--- app/src/main/java/com/example/a20230140083_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a20230140083_roomdatabase/repositori/ContainerApp.kt
@@ -4,3 +4,7 @@ import android.app.Application
 import android.content.Context
 import com.example.a20230140083_roomdatabase.room.DatabaseSiswa
 
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
