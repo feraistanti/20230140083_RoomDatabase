1203ee6671d9bd63bf4225bcc88ab7fcf99974a6
feraistanti <fistanti551@gmail.com>
2025-11-27 16:06:11 +0700

    Database Siswa Room

--- app/src/main/java/com/example/a20230140083_roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/a20230140083_roomdatabase/room/DatabaseSiswa.kt
@@ -13,14 +13,6 @@ import androidx.room.RoomDatabase
         @Volatile
         private var Instance: DatabaseSiswa? = null
 
-        fun getDatabase(context: Context): DatabaseSiswa {
-            return (Instance?: synchronized(this){
-                Room.databaseBuilder(
-                    context, DatabaseSiswa::class.java,
-                    "siswa_database")
-                    .build().also { Instance=it }
-            })
-        }
     }
 
 }
