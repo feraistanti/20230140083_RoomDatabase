7854505e81b3c8b4bd8646047eaa2e41bdddf30a
feraistanti <fistanti551@gmail.com>
2025-11-27 16:10:42 +0700

    Interface destinasi navigasi

--- app/src/main/java/com/example/a20230140083_roomdatabase/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/a20230140083_roomdatabase/view/route/DestinasiNavigasi.kt
@@ -1,6 +1,6 @@
 package com.example.a20230140083_roomdatabase.view.route
 
 interface DestinasiNavigasi {
-    val route: String
+    val route: String //Interface destinasi navigasi
     val titleRes: Int
 }
